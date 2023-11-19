b5ae1b20470c96119061319661c499ff56c2bfbf
Niels Gandraß <niels@gandrass.de>
2023-11-19 12:52:17 +0100

    Integrate login status into AppBar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,23 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
+    <v-app-bar flat>
+        <v-app-bar-title>
+            <router-link to="/" class="text-decoration-none text-muted">
+                <v-icon icon="mdi-circle-slice-6"/>
+                EF-RMS
+            </router-link>
+        </v-app-bar-title>
 
-      Essentials Preset
-    </v-app-bar-title>
-  </v-app-bar>
+        <v-btn v-if="!authStore.isLoggedIn" to="/login" text>Login</v-btn>
+        <router-link v-if="authStore.isLoggedIn" to="/login">
+            <v-avatar color="grey" size="36">
+                <span class="white--text headline">{{authStore.username.slice(0, 2).toUpperCase()}}</span>
+            </v-avatar>
+        </router-link>
+    </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-  //
+    import {useAuthStore} from "@/store/auth";
+
+    const authStore = useAuthStore();
 </script>
