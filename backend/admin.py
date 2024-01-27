0245b1f0e7dee52db3bacbf0e83d98b5ed5e3c76
Niels Gandraß <niels@gandrass.de>
2024-01-27 13:07:11 +0100

    Define proper Django admin pages for models

--- backend/admin.py
+++ backend/admin.py
@@ -3,17 +3,88 @@ from django.contrib import admin
 from backend.models import *
 
 
-admin.site.register(binding.ItemBinding)
-admin.site.register(callbox.Callbox)
-admin.site.register(callbox.CallboxTemplate)
-admin.site.register(eventlog.EventLogEntry)
-admin.site.register(item.Item)
-admin.site.register(item.ItemTemplate)
-admin.site.register(phone.Phone)
-admin.site.register(phone.PhoneTemplate)
-admin.site.register(radio.RadioAccessory)
-admin.site.register(radio.RadioAccessoryTemplate)
-admin.site.register(radio.RadioDevice)
-admin.site.register(radio.RadioDeviceTemplate)
-admin.site.register(user.User)
-admin.site.register(user.ItemOwner)
+admin.site.register(binding.ItemBinding)  # TODO
+admin.site.register(eventlog.EventLogEntry)  # TODO
+
+
+@admin.register(item.ItemOwner)
+class ItemOwnerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'shortname']
+    search_fields = ['name']
+    ordering = ['name']
+
+
+@admin.register(item.ItemTemplate, callbox.CallboxTemplate, phone.PhoneTemplate, radio.RadioAccessoryTemplate, radio.RadioDeviceTemplate)
+class ItemTemplateAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'owner']
+    search_fields = ['name', 'description', 'owner']
+    ordering = ['name', 'owner']
+
+
+@admin.register(item.Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['get_name', 'get_owner', 'serialnumber', 'notes', 'handed_out']
+    search_fields = ['get_name', 'get_owner', 'serialnumber', 'notes']
+
+    @admin.display(description="Name", ordering='template__name')
+    def get_name(self, obj):
+        return obj.template.name
+
+    @admin.display(description="Owner", ordering='template__owner__name')
+    def get_owner(self, obj):
+        return obj.template.owner.name
+
+
+@admin.register(callbox.Callbox)
+class CallboxAdmin(admin.ModelAdmin):
+    list_display = ['get_name', 'notes', 'location', 'extension', 'network', 'has_camera']
+    search_fields = ['get_name', 'notes', 'location', 'extension']
+
+    @admin.display(description="Name", ordering='template__name')
+    def get_name(self, obj):
+        return obj.template.name
+
+
+@admin.register(phone.Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['get_name', 'notes', 'location', 'extension', 'network', 'dhcp']
+    search_fields = ['get_name', 'notes', 'location', 'extension']
+
+    @admin.display(description="Name", ordering='template__name')
+    def get_name(self, obj):
+        return obj.template.name
+
+
+@admin.register(radio.RadioAccessory)
+class RadioAccessoryAdmin(admin.ModelAdmin):
+    list_display = ['get_name', 'get_owner', 'serialnumber', 'notes', 'handed_out']
+    search_fields = ['get_name', 'get_owner', 'serialnumber', 'notes']
+
+    @admin.display(description="Name", ordering='template__name')
+    def get_name(self, obj):
+        return obj.template.name
+
+    @admin.display(description="Owner", ordering='template__owner__name')
+    def get_owner(self, obj):
+        return obj.template.owner.name
+
+
+@admin.register(radio.RadioDevice)
+class RadioDeviceAdmin(admin.ModelAdmin):
+    list_display = ['get_name', 'get_owner', 'callsign', 'serialnumber', 'notes', 'handed_out']
+    search_fields = ['get_name', 'get_owner', 'callsign', 'serialnumber', 'notes']
+
+    @admin.display(description="Name", ordering='template__name')
+    def get_name(self, obj):
+        return obj.template.name
+
+    @admin.display(description="Owner", ordering='template__owner__name')
+    def get_owner(self, obj):
+        return obj.template.owner.name
+
+
+@admin.register(user.User)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['username', 'ef_reg_id', 'ef_security_collar_id', 'last_seen', 'is_active', 'is_staff', 'is_superuser']
+    search_fields = ['username', 'ef_reg_id', 'ef_security_collar_id']
+    ordering = ['username']
