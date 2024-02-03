a45ad62efa5c13f3453f62f30c935c50c7fe4433
Niels Gandraß <niels@gandrass.de>
2024-02-03 13:43:44 +0100

    frontend: Add leaflet demo component for deployment map

--- src/views/DeploymentMap.vue
+++ src/views/DeploymentMap.vue
@@ -1,3 +1,44 @@
 <template>
     <h1>Deployment Map</h1>
+    <l-map
+        ref="map"
+        v-model:zoom="zoom"
+        :center="[65, 802]"
+        :use-global-leaflet="false"
+        :options="options"
+    >
+        <l-image-overlay
+            url="/src/assets/deployment-map.svg"
+            :bounds="bonds"
+        ></l-image-overlay>
+    </l-map>
 </template>
+
+<script lang="ts">
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LImageOverlay } from "@vue-leaflet/vue-leaflet";
+
+export default {
+    components: {
+        LImageOverlay,
+        LMap,
+        LTileLayer,
+    },
+
+    data() {
+        return {
+            zoom: 3.0,
+            options: {
+                zoomDelta: 0.5,
+                zoomSnap: 0.5,
+            },
+        };
+    },
+
+    computed: {
+        bonds() {
+            return [[0, 0], [1281, 1605]];
+        },
+    },
+};
+</script>
