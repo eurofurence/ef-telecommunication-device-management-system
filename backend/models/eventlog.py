6a6bdbba867b1179f423d5d0a8dcd523a5c519b8
Niels Gandraß <niels@gandrass.de>
2024-03-03 21:28:26 +0100

    backend: Define additional EventLogEntry Actions

--- backend/models/eventlog.py
+++ backend/models/eventlog.py
@@ -10,6 +10,14 @@ class EventLogEntry(models.Model):
 
     class Action(models.TextChoices):
         USER_LOGIN = 'USER_LOGIN', 'User logged in'
+        CREATE_ITEM = 'CREATE_ITEM', 'Item created'
+        DELETE_ITEM = 'DELETE_ITEM', 'Item deleted'
+        UPDATE_ITEM = 'UPDATE_ITEM', 'Item updated'
+        CREATE_ITEM_TEMPLATE = 'CREATE_ITEM_TEMPLATE', 'Item template created'
+        DELETE_ITEM_TEMPLATE = 'DELETE_ITEM_TEMPLATE', 'Item template deleted'
+        UPDATE_ITEM_TEMPLATE = 'UPDATE_ITEM_TEMPLATE', 'Item template updated'
+        CREATE_ITEM_BINDING = 'CREATE_ITEM_BINDING', 'Item binding created'
+        DELETE_ITEM_BINDING = 'DELETE_ITEM_BINDING', 'Item binding deleted'
 
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     timestamp = models.DateTimeField(
@@ -31,3 +39,14 @@ class EventLogEntry(models.Model):
         default=None,
         help_text="Additional data optionally associated with the event"
     )
+
+    @classmethod
+    def log(cls, user, action, data=None):
+        """
+        Logs an event.
+
+        :param user: User that performed the action
+        :param action: Action that was performed
+        :param data: Additional data optionally associated with the event
+        """
+        cls.objects.create(user=user, action=action, data=data)
