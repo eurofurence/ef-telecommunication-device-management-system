9195471a2788aceb7df19a0922fbcd92b655ac22
Niels Gandraß <niels@gandrass.de>
2024-03-07 17:13:06 +0100

    frontend: Create proper types for EventLogEntries

--- src/types/LogEvent.ts
+++ src/types/LogEvent.ts
@@ -4,14 +4,148 @@
 export type LogEvent = {
     id: number;
     date: string;
-    type: LogEventType;
+    user: string;
+    type: LogEventTypeDeclaration;
     message: string;
-    description?: string;
+    description: string;
 }
 
-export type LogEventType = {
+/**
+ * Interface contract for LogEventType class
+ */
+type LogEventTypeDeclaration = {
     key: string;
     label: string;
     color: string;
     icon: string;
+    description(data: any): string;
+}
+
+/**
+ * Log event types
+ */
+export class LogEventType {
+
+    static readonly UNKNOWN: LogEventTypeDeclaration = {
+        key: 'UNKNOWN',
+        label: 'Unknown',
+        color: 'grey',
+        icon: 'mdi-help',
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        description(data: any): string {
+            return 'Unknown event';
+        }
+    }
+
+    static readonly USER_LOGIN: LogEventTypeDeclaration = {
+        key: 'USER_LOGIN',
+        label: 'User logged in',
+        color: 'purple-lighten-2',
+        icon: 'mdi-login',
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        description(data: any): string {
+            return ``;
+        }
+    }
+
+    static readonly CREATE_ITEM: LogEventTypeDeclaration = {
+        key: 'CREATE_ITEM',
+        label: 'Item created',
+        color: 'green-lighten-1',
+        icon: 'mdi-plus',
+        description(data: any): string {
+            return `TODO: ${data}`;
+        }
+    }
+
+    static readonly DELETE_ITEM: LogEventTypeDeclaration = {
+        key: 'DELETE_ITEM',
+        label: 'Item deleted',
+        color: 'red-lighten-1',
+        icon: 'mdi-minus',
+        description(data: any): string {
+            return `TODO: ${data}`;
+        }
+    }
+
+    static readonly UPDATE_ITEM: LogEventTypeDeclaration = {
+        key: 'UPDATE_ITEM',
+        label: 'Item updated',
+        color: 'orange-darken-1',
+        icon: 'mdi-pencil',
+        description(data: any): string {
+            return `TODO: ${data}`;
+        }
+    }
+
+    static readonly CREATE_ITEM_TEMPLATE: LogEventTypeDeclaration = {
+        key: 'CREATE_ITEM_TEMPLATE',
+        label: 'Item template created',
+        color: 'green-lighten-1',
+        icon: 'mdi-toy-brick-plus-outline',
+        description(data: any): string {
+            return `TODO: ${data}`;
+        }
+    }
+
+    static readonly DELETE_ITEM_TEMPLATE: LogEventTypeDeclaration = {
+        key: 'DELETE_ITEM_TEMPLATE',
+        label: 'Item template deleted',
+        color: 'red-lighten-1',
+        icon: 'mdi-toy-brick-remove-outline',
+        description(data: any): string {
+            return `TODO: ${data}`;
+        }
+    }
+
+    static readonly UPDATE_ITEM_TEMPLATE: LogEventTypeDeclaration = {
+        key: 'UPDATE_ITEM_TEMPLATE',
+        label: 'Item template updated',
+        color: 'orange-darken-1',
+        icon: 'mdi-toy-brick-outline',
+        description(data: any): string {
+            return `TODO: ${data}`;
+        }
+    }
+
+    static readonly CREATE_ITEM_BINDING: LogEventTypeDeclaration = {
+        key: 'CREATE_ITEM_BINDING',
+        label: 'Item binding created',
+        color: 'green-lighten-1',
+        icon: 'mdi-basket-plus-outline',
+        description(data: any): string {
+            return `To user: ${data.user.pretty_name} - Item: ${data.item}}`;
+        }
+    }
+
+    static readonly DELETE_ITEM_BINDING: LogEventTypeDeclaration = {
+        key: 'DELETE_ITEM_BINDING',
+        label: 'Item binding deleted',
+        color: 'red-lighten-1',
+        icon: 'mdi-basket-minus-outline',
+        description(data: any): string {
+            return `From user: ${data.user.pretty_name} - Item: ${data.item}}`;
+        }
+    }
+
+    static readonly UPDATE_ITEM_BINDING: LogEventTypeDeclaration = {
+        key: 'UPDATE_ITEM_BINDING',
+        label: 'Item binding updated',
+        color: 'orange-darken-1',
+        icon: 'mdi-basket-outline',
+        description(data: any): string {
+            return `TODO: ${data}`;
+        }
+    }
+
+    /**
+     * Returns the LogEventTypeDeclaration for the given key or UNKNOWN
+     *
+     * @param key of the LogEventType
+     * @return LogEventTypeDeclaration for the given key
+     */
+    static get(key: string): LogEventTypeDeclaration {
+        return <LogEventTypeDeclaration>LogEventType[key as keyof typeof LogEventType] ?? LogEventType.UNKNOWN;
+    }
+
 }
